1a1986244aa64ca182e42cdfaaf4ea17a010382a
James Huston <jamzhuston@gmail.com>
2023-11-16 22:16:11 -0700

    Added name, and header details to RoomList.hpp

---
+++ include/RoomList.hpp
@@ -0,0 +1,28 @@
+/**
+* @Author James Huston [huston@uleth.ca]
+* @date 2023-11
+*
+*
+*
+*
+*
+*/
+
+#ifndef ROOMLIST.HPP
+#define ROOMLIST.HPP
+
+class {
+public:
+
+
+
+
+private:
+
+
+
+
+
+};
+
+#endif
