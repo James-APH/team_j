f7d619d39a2bcbd9013ed41184d0f31ee204a6cd
James Huston <jamzhuston@gmail.com>
2023-11-16 21:57:33 -0700

    Added name, and header details to Item.hpp

---
+++ include/Item.hpp
@@ -0,0 +1,25 @@
+/* @Authors - James Huston | jamzhuston@gmail.com |
+ *          - 
+ *          -
+ * Created 10/30/2023
+ *
+*/
+
+#ifndef ITEM.HPP
+#define ITEM.HPP
+
+#include <iostream>
+#include <string>
+
+
+
+
+class item {
+public:
+
+
+
+private:
+};
+
+endif;
