3cc0eb80b4ae05c1ee01704a4906d2df0e7e9459
James Huston <jamzhuston@gmail.com>
2023-11-16 22:01:13 -0700

    Added name, and header details to Character.hpp

---
+++ include/Character.hpp
@@ -0,0 +1,27 @@
+/**
+* @Author James Huston [huston@uleth.ca]
+* @date 2023-11
+*
+*
+*
+*
+*/
+
+#ifndef CHARACTER.HPP
+#define CHARACTER.HPP
+
+
+class Character {
+public:
+
+
+
+private:
+
+
+
+};
+
+#endif
+
+
