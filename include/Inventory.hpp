d256e23ff20e63ace90642bfa17070cf6db3a81f
James Huston <jamzhuston@gmail.com>
2023-11-16 21:54:37 -0700

    added name, header details to Inventory.hpp

---
+++ include/Inventory.hpp
@@ -0,0 +1,26 @@
+/* @Author: James Huston | jamzhuston@gmail.com
+*  Created: 10/30/2023
+*
+*/
+
+#ifndef INVENTORY.HPP
+#define INVENTORY.HPP
+
+class Inventory {
+public:
+
+
+
+
+
+
+
+private:
+
+
+
+
+
+}
+
+#endif
