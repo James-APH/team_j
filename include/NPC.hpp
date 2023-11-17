2cdfb754a33780ba8643bdf318ae7d5d4962ff60
James Huston <jamzhuston@gmail.com>
2023-11-16 22:04:03 -0700

    Added name, and header details to NPC.hpp

---
+++ include/NPC.hpp
@@ -0,0 +1,30 @@
+/**
+* @Author James Huston [huston@uleth.ca]
+* @date 2023-11
+*
+*
+*
+*
+*
+*/
+
+
+#ifndef NPC.HPP
+#define NPC.HPP
+
+class NPC {
+public:
+
+
+
+
+
+
+private:
+
+
+
+};
+
+
+#endif
