2c7c9480c37f8c63cc57e5dd9be377b5ba5c9487
James Huston <jamzhuston@gmail.com>
2023-11-16 22:46:09 -0700

    implemented functions

---
+++ src/RoomState.cpp
@@ -0,0 +1,49 @@
+/**
+* @author James Huston [huston@uleth.ca]
+* @date 11/2023
+*/
+
+#include "roomstate.hpp"
+
+
+FullyExploredRoom::FullyExploredRoom() {}
+  
+FullyExploredRoom::~FullyExploredRoom() {}
+  
+bool FullyExploredRoom::isExplored() {
+  return true;
+}
+  
+bool FullyExploredRoom::allSecretsFound() {
+  return true; 
+}
+
+
+
+ExploredRoom::ExploredRoom() {}
+  
+ExploredRoom::~ExploredRoom() {}
+
+bool ExploredRoom::isExplored() {
+  return true;
+}
+
+bool ExploredRoom::allSecretsFound() {
+  return false;
+}
+
+
+
+
+UnexploredRoom::UnexploredRoom() {}
+  
+UnexploredRoom::~UnexploredRoom() {}
+  
+bool UnexploredRoom::isExplored() {
+  return false;
+}
+  
+bool UnexploredRoom::allSecretsFound() {
+  return false
+}
+
