fba351ce9e0d8350738be8efbb611e7aa712ff03
James Huston <jamzhuston@gmail.com>
2023-11-16 22:45:22 -0700

    Added name, and header details in Player.hpp

---
+++ include/Player.hpp
@@ -0,0 +1,29 @@
+/**
+* @Author James Huston [huston@uleth.ca]
+* @date 2023-11
+*
+*
+*
+*
+*
+*/
+
+#ifndef PLAYER.HPP
+#define PLAYER.HPP
+
+class Player {
+public:
+
+
+
+
+
+private:
+
+
+
+
+
+};
+
+#endif
