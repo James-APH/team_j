d8cb6b3bbe2c087d0dd62c6c512b2455b9e83692
James Huston <jamzhuston@gmail.com>
2023-11-16 22:13:56 -0700

    Added name, and header details to Room.hpp

---
+++ include/Room.hpp
@@ -0,0 +1,27 @@
+/**
+* @Author James Huston [huston@uleth.ca]
+* @date 2023-11
+*
+*
+*
+*
+*/
+
+#ifndef ROOM.HPP
+#define ROOM.HPP
+
+class Room {
+public:
+
+
+
+
+
+
+private:
+
+
+
+};
+
+#endif
