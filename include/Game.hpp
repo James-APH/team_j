d5b09f6da18623b2adc74425f5c8a39dc2c1740a
James Huston <jamzhuston@gmail.com>
2023-11-16 22:09:38 -0700

    Added name, and header details to Game.hpp

---
+++ include/Game.hpp
@@ -0,0 +1,29 @@
+/**
+* @Author James Huston [huston@uleth.ca]
+* @date 2023-11
+*
+*
+*
+*
+*
+*
+*/
+
+#ifndef GAME.HPP
+#define GAME.HPP
+
+class Game {
+public:
+
+
+
+
+
+private:
+
+
+
+};
+
+
+#endif
