83abaa8e12bd6f276fe18eebc1fc6d97bdaed5f9
James Huston <jamzhuston@gmail.com>
2023-11-26 20:21:53 -0700

    Feat: commented all code to doxygen standards in Inventory.hpp

--- include/Inventory.hpp
+++ include/Inventory.hpp
@@ -11,40 +11,84 @@
 
 #include "Item.hpp"
 
-
+/**
+* @class Inventory in Inventory.hpp "Inventory.hpp"
+* @brief class for monitoring and maintaining the items
+* any user has access to.
+*/
 class Inventory {
   public:
 
+  /**
+  * @brief default constructor
+  */
   Inventory();
 
-
+  /**
+  * @brief construction from vector
+  */
   Inventory(std::vector<Item> itemList);
 
+  /**
+  * @brief destructor
+  */
+  ~Inventory();
 
+  /**
+  * @brief outputs the description of one item
+  * @param[in] itemName the name of the item
+  */
   void Inspect(std::string itemName);
 
-
+  /**
+  * @brief allows user to accept items from npcs and take items from rooms
+  * @param[in] itemName the name of the item
+  */
   void addItem(std::string itemName);
 
-
+  /**
+  * @brief removes item from list
+  * @param[in] itemName the name of the item
+  */
   void deleteItem(std::string itemName);
 
-
+  /**
+  * @brief checks if item exists in list
+  * @param[in] itemName the name of the item
+  * @return true if item is in list false otherwise
+  */
   bool hasItem(std::string itemName);
 
 
+  /**
+  * @brief returns an item that exists in the list
+  * @param[in] itemName the name of the item
+  * @return A reference to an item
+  */
   Item& getItem(std::string itemName);
 
-  
+  /**
+  * @brief outputs the information created in
+  * the toString function.
+  */
+  void display();
+
+
   private:
   std::vector<Item> itemList;
   int size;
 
-  std::toString();
+  /**
+  * @brief creates a string formatted to list
+  * all the items in inventory in a nice way
+  * @return string, string of items
+  */
+  std::string toString();
 
+  /**
+  * @brief adds a description to a newly added item
+  */
   void addItemDescription(std::string fileName);
-
-
 };
 
 #endif
