bf852c47b0053bfb7d49cc193eb69f9f99a4302d
James Huston <jamzhuston@gmail.com>
2023-11-28 13:45:59 -0700

    Feat: started planning how the game class will work

--- include/Game.h
+++ include/Game.h
@@ -11,8 +11,33 @@
 
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
+/**
+ * @class Game in Game.h "Game.h"
+ * @details this class will handle
+ * the majority of the game logic, i.e
+ * the output the user will actually see
+ * for future james, in this class
+ * we will add a function to start the 
+ * game, a function to output splash screens
+ * a function to play the game
+ * a function to build all of the npcs
+ * a function to build all of the rooms
+ * a list of all of the rooms (I may use 
+ * another class to manage this)
+ */
+class Game {
+ public:
+  /**
+   * @brief default constructor
+   */
+  Game();
+ 
 
-class Game {};
+ private:
+
+
+
+};
 
 
 #endif
