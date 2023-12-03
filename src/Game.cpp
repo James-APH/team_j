06f29f99a298ef5c8d1c9f22adad2be67ea96e8a
James Huston <jamzhuston@gmail.com>
2023-12-03 11:39:41 -0700

    Commented out code in Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,44 +1,44 @@
-#include <Room.h>
-#include <Item.h>
-#include <Puzzle.h>
-#include <Character.h>
+// #include <Room.h>
+// #include <Item.h>
+// #include <Puzzle.h>
+// #include <Character.h>
 
 
-Item* key1 = new Item("Attic Key", "");
-Item* key2 = new Item("Hallway Key", "");
-Item* key3 = new Item("LivingRoom Key", "");
-Item* will = new Item("will", "");
-Item* knife = new Item("knife", "");
+// Item* key1 = new Item("Attic Key", "");
+// Item* key2 = new Item("Hallway Key", "");
+// Item* key3 = new Item("LivingRoom Key", "");
+// Item* will = new Item("will", "");
+// Item* knife = new Item("knife", "");
 
-NPC* witness = new NPC();
-NPC* brent = new NPC();
-NPC* officer = new NPC();
+// NPC* witness = new NPC();
+// NPC* brent = new NPC();
+// NPC* officer = new NPC();
 
-Player* user = new Player("James");
-
-
-Puzzle* HallWayOnePuzzle = new ItemPuzzle("need a key to access bedroom");
-Puzzle* HallwayTwoPuzzle = new ItemPuzzle("need a key to access study");
-Puzzle* MysteryRoomPuzzle = new DialoguePuzzle("deciepher the code to access ");
-Puzzle* LibraryPuzzle = new ThinkingPuzzle("deceipher the code");
-Puzzle* AtticPuzzle = new ItemPuzzle("need a key to access family picture");
+// Player* user = new Player("James");
 
 
+// Puzzle* HallWayOnePuzzle = new ItemPuzzle("need a key to access bedroom");
+// Puzzle* HallwayTwoPuzzle = new ItemPuzzle("need a key to access study");
+// Puzzle* MysteryRoomPuzzle = new DialoguePuzzle("deciepher the code to access ");
+// Puzzle* LibraryPuzzle = new ThinkingPuzzle("deceipher the code");
+// Puzzle* AtticPuzzle = new ItemPuzzle("need a key to access family picture");
 
 
 
-Room* Garden = new DialogueRoom("Garden");
-Room* CoatCloset = new ItemRoom("CoatCloset");
-Room* BallRoom = new DialogueRoom("BallRoom");
-Room* HallWay = new ItemPuzzleRoom("HallWay");
-Room* BedRoom = new ItemRoom("BedRoom");
-Room* Library = new ThinkingPuzzleRoom("Library");
-Room* MysteryRoom = new ThinkingPuzzleRoom("MysteryRoom");
-Room* livingRoom = new ItemPuzzleRoom("livingRoom");
-Room* Study = new ItemRoom("Study");
-Room* Stairway = new DialogueRoom("Stairway");
-Room* Cellar = new DialogueRoom("Cellar");
-Room* Attic = new ItemPuzzleRoom("Attic");
 
 
-vector<Room*> roomlist();
+// Room* Garden = new DialogueRoom("Garden");
+// Room* CoatCloset = new ItemRoom("CoatCloset");
+// Room* BallRoom = new DialogueRoom("BallRoom");
+// Room* HallWay = new ItemPuzzleRoom("HallWay");
+// Room* BedRoom = new ItemRoom("BedRoom");
+// Room* Library = new ThinkingPuzzleRoom("Library");
+// Room* MysteryRoom = new ThinkingPuzzleRoom("MysteryRoom");
+// Room* livingRoom = new ItemPuzzleRoom("livingRoom");
+// Room* Study = new ItemRoom("Study");
+// Room* Stairway = new DialogueRoom("Stairway");
+// Room* Cellar = new DialogueRoom("Cellar");
+// Room* Attic = new ItemPuzzleRoom("Attic");
+
+
+// vector<Room*> roomlist();
