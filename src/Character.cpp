6cb73b664a245bc85ab1ac03d78339d2f04c1aa9
James Huston <jamzhuston@gmail.com>
2023-12-05 13:12:39 -0700

    Feat & Fix: removed drop param, and added code to ask user what they'd like to drop, also impemented inspectItem and listInventory.

--- src/Character.cpp
+++ src/Character.cpp
@@ -10,7 +10,6 @@
 #include <vector>
 
 #include "Item.h"
-#include "Inventory.h"
 #include "Character.h"
 
 
@@ -47,41 +46,67 @@ void NPC::display() {
   std::cout << stringReader.str() << std::endl;
 }
 
-Player::Player(std::string name, const Item& item) : Character(name) {
+Player::Player(std::string name, const Item& item)
+  : Character(name) {
   itemList.push_back(new Item(item));
 }
 
 Player::~Player() {
-
+  for (auto it : itemList) {
+    if (it != nullptr)
+      delete it;
+  }
+  itemList.clear();
 }
 
 void Player::pickUp(const Item& item) {
-  if(std::find(itemList.begin(), itemList.end(), item) == itemList.end()) {
+  if(std::find(itemList.begin(), itemList.end()
+  , item) == itemList.end()) {
     itemList.push_back(new Item(item));
   }
 }
 
-void Player::drop(std::string name) {
-  bool deleted;
-  for(int i = 0; i < itemList.size(); i++) {
-    if(itemList[i]->getName() == name) {
-      deleted = true;
-      
+void Player::drop() {
+  std::string itemName;
+  std::cout << "What item would you like to drop?" << std::endl;
+  for (auto it : itemList) {
+    if (it->getName() == itemName) {
+      itemList.remove(it);
+      std::cout << "Item dropped" << std::endl;
+      return;
+    }
+    std::cout << "Item not found!" << std::endl;
+  }
+}
+
+void Player::InspectItem() {
+  std::string itemName;
+  std::cout << "What item would you like to inspect?" << std::endl;
+  for (auto it : itemList) {
+    if (it->getName() == itemName) {
+      std::cout << it->toString() << std::endl;
+      return;
     }
   }
-
-    
-    std::cout << "Item Dropped" << std::endl;
-  }
-  std::cout << "Item not in inventory" << std::endl;
+  std::cout << "Item not found!" << std::endl;
 }
+
+void Player::listInventory() {
+  for (auto it : itemList) {
+    std::cout << it->toString() << std::endl;
+  }
+}
+
 
 Item& Player::useItem() {
   std::string itemName;
-  do {
-    std::cout << "What item would you like to use? " << std::endl;
-    std::cout << inventory->toString() << std::endl;
-    std::cin >> itemName;
-  } while (!inventory->hasItem(itemName));
-  return inventory->getItem(itemName);
+  std::cout << "What item would you like to use?" << std::endl;
+  std::cin >> itemName;
+  for (auto it : itemList) {
+    if (it->getName() == itemName) {
+      return *it;
+    }
+  }
+  std::cout << "Item not found!" << std::endl;
+  return;
 }
