38bac78f5bbbc931b41d9332785dc8f7d503390f
Raven Huery <raven.huery@uleth.ca>
2023-12-08 18:42:37 -0700

    More changes to make unit tests work

--- test/NPCTest.cpp
+++ test/NPCTest.cpp
@@ -15,14 +15,14 @@
 
 
 TEST(TestNPC, defaultNPCConstructorTest) {
-    NPC* anon = new NPC("Robert", "Lol");
-    EXPECT_EQ(anon->getName(), "Robert");
+    NPC* anon = new NPC("Craig", "Lol");
+    EXPECT_EQ(anon->getName(), "Craig");
 
     delete anon;
 }
 
 TEST(TestNPC, copyNPCConstructorTest) {
-    NPC* solidAnon = new NPC("Jhonfrey", "Ur ded kiddo :3");
+    NPC* solidAnon = new NPC("Rose", "Ur ded kiddo :3");
     NPC* liquidAnon = new NPC(*solidAnon);
 
     EXPECT_EQ(liquidAnon->getName(), "Jhonfrey");
